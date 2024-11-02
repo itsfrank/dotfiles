9dc8f11fad556ec60bca6717ec6f6a76d7e39d34
Frank O'Brien <fobrien@roblox.com>
2024-11-02 16:16:55 -0700

    format, lint tms

--- tmux/scripts/tms.sh
+++ tmux/scripts/tms.sh
@@ -4,21 +4,21 @@ if [[ $# -eq 1 ]]; then
     selected=$1
 else
     selected=$(find \
-            ~/git \
-            ~/git/roblox \
-            ~/git/roblox/utils \
-            ~/git/roblox/misc \
-            ~/git/misc \
-            ~/frk \
-            ~/frk/go \
-            ~/frk/nvim-plugins \
-            ~/misc \
+        ~/git \
+        ~/git/roblox \
+        ~/git/roblox/utils \
+        ~/git/roblox/misc \
+        ~/git/misc \
+        ~/frk \
+        ~/frk/go \
+        ~/frk/nvim-plugins \
+        ~/misc \
         -mindepth 1 -maxdepth 1 -type d)
     selected+="\n"
     selected+=$(find ~/git/roblox/ge-worktrees -mindepth 2 -maxdepth 2 -type d)
     selected+="\n"
     selected+="$HOME/.config"
-    selected=$(printf "$selected" | fzf)
+    selected=$(printf '%s' "$selected" | fzf)
 fi
 
 if [[ -z $selected ]]; then
@@ -26,13 +26,12 @@ if [[ -z $selected ]]; then
 fi
 
 selected_name=$(basename "$selected" | tr . _)
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 if [[ -z $TMUX ]]; then
-    tmux attach-session -t $selected_name
+    tmux attach-session -t "$selected_name"
 else
-    tmux switch-client -t $selected_name
+    tmux switch-client -t "$selected_name"
 fi
-
