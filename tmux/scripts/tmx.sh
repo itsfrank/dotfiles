3f1372e0a995655f2bc351aa59df6b5984180157
Frank O'Brien <fobrien@roblox.com>
2024-11-05 15:31:30 -0800

    add tms keymap to tmx

--- tmux/scripts/tmx.sh
+++ tmux/scripts/tmx.sh
@@ -2,7 +2,7 @@
 
 # given one line output from tmux list-sessions
 function _get_session_name {
-    local result=$(awk '{print substr($1, 1, length($1)-1)}' <<< "$1")
+    local result=$(awk '{print substr($1, 1, length($1)-1)}' <<<"$1")
     echo "$result"
 }
 
@@ -14,12 +14,15 @@ function _tmx {
 
     export -f _get_session_name
     export -f _exec
-    local choice=$( tmux list-sessions | SHELL=/bin/bash fzf \
+    local choice=$(
+        tmux list-sessions | SHELL=/bin/bash fzf \
             --layout=reverse \
             --bind='ctrl-x:execute(_exec {})+reload(tmux list-sessions)' \
-            --expect="CTRL-n" \
+            --expect="ctrl-n" \
+            --expect="ctrl-s" \
+            --expect="ctrl-i" \
             --bind='tab:down,btab:up' \
-            --header="ENTER switch, CTRL-X kill, CTRL-N new"  --color=header:#719872
+            --header="ENTER switch, <c-x> kill, <c-n> new, <c-s> tms" --color=header:#719872
     )
 
     if [[ "$choice" == "" ]]; then
@@ -28,14 +31,17 @@ function _tmx {
 
     # check if new session requested
     local line1=$(echo "$choice" | head -n 1)
-    if [[ "$line1" == "CTRL-n" ]]; then
-        read -p "session name: " session_name
+    if [[ "$line1" == "ctrl-n" ]]; then
+        read -p "session name: " session_name || return 0
         if [ -z "${TMUX}" ]; then
             tmux new-session -s "$session_name"
         else
             tmux new-session -d -s "$session_name"
             tmux switch-client -t "$session_name"
         fi
+        return 0
+    elif [[ "$line1" == "ctrl-s" ]]; then
+        ~/.config/tmux/scripts/tms.sh || return 0
         return 0
     fi
 
