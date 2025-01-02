5685963100f47b173d0ae4aa422fce76bdca27a6
itsfrank <frankobrien11@yahoo.ca>
2025-01-02 15:23:12 -0500

    merge nix system files

---
+++ nix/flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "frank's nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+    }:
+    let
+      configuration =
+        { pkgs, config, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.home-manager
+          ];
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "mas"
+              "pkl"
+            ];
+            casks = [
+              "ghostty"
+              "wezterm"
+              "zen-browser"
+              "karabiner-elements"
+            ];
+            masApps = {
+              # use `mas search tof ind them`
+              # "Yoink" = 457622435;
+            };
+            onActivation.cleanup = "zap";
+          };
+
+          fonts.packages = [
+            (pkgs.nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+          ];
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          programs.zsh.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."frk" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "frk";
+            };
+          }
+        ];
+      };
+    };
+}
